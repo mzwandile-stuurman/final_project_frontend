e5a7467d71c4a5d52e4d3d6e304f9a6293698650
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2021-08-31 10:04:00 +0200

    registery

---
+++ register.js
@@ -0,0 +1,30 @@
+// function to register user
+function register() {
+    fetch("https://still-brushlands-23193.herokuapp.com/users/", {
+      method: "POST",
+      body: JSON.stringify({
+        first_name: document.getElementById("first_name").value,
+        last_name: document.getElementById("last_name").value,
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+        address: document.getElementById("address").value,
+        phone_number: document.getElementById("phone_number").value,
+        user_email: document.getElementById("user_email").value,
+      }),
+      headers: {
+        "Content-type": "application/json"
+        
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data["status_code"] == 200) {
+          alert("Registered successfully!, please log in.");
+          window.location.href = "./index.html";
+        } else {
+          alert("Please enter correct information");
+          
+        }
+      });
+  }

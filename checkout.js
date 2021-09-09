942cf58fbf0ec3339c28b1d32c3199a89743027a
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2021-09-09 09:28:34 +0200

    pushing checkout

--- checkout.js
+++ checkout.js
@@ -10,19 +10,37 @@ function showCart(){
         storedCartitems.forEach((product) => {
   
         product_container.innerHTML += `
-  
-            <div class = "products">
+        <div class = "products">  
+            <div class="image-div">
                 <img src="${product.image}" class = "product-image">
+            </div>
+            <div class="cart-content">
                 <div class = "product-content"> 
+          
                     <h4 class = "product-title"> ${product.product_name}</h4>
                     <p class = "product-description"> ${product.brand}</p>
                     <p class = "product-price">R${product.price} </p>
-                    <button onclick="addToCart(${product.prod_id})">Add to Cart</button>
-                
                 </div>
-                
+
+                <div class="cart-btns">
+                    <button class ="revome_cart" onclick="removeItem(${product.prod_id})">Remove item</button>
+                    <button class="checkout-button" onlcick="">Checkout</button>
+                    </div>
+
+                <div class="quantity-container">
+                    <input type="button" onclick="decrementValue()" value="-" />
+                    <input type="text" name="quantity" value="1" maxlength="2" max="10" size="1" id="number" />
+                    <input type="button" onclick="incrementValue()" value="+" />
+                </div>
             </div>
-            `;
+            
+        </div>
+            
+          
+
+  
+            
+        `;
         });
     }else{
         product_container.innerHTML = "<h2> No items in cart</h2>";

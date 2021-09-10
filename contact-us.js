cc7af17849e5bb1f6bfbf82590b74ea6708b972f
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2021-09-10 13:50:40 +0200

    contact page

--- contact-us.js
+++ contact-us.js
@@ -1,6 +1,31 @@
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-});
+function contact() {
+    fetch("https://still-brushlands-23193.herokuapp.com/contact-us/", {
+      method: "POST",
+      body: JSON.stringify({
+        name: document.getElementById("name").value,
+        email_address: document.getElementById("email").value,
+        enquiry: document.getElementById("message").value,
+       
+      }),
+      headers: {
+        "Content-type": "application/json"
+        
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data["status_code"] == 201) {
+          alert("Thank you for contacting us, you will get a response soon.");
+          window.location.href = "./home_logged_in.html";
+        } else {
+          alert("Sorry something went wrong please try again");
+          location.reload();
+        }
+      });
+  }
+
+function logOut(){
+    localStorage.clear()
+    window.location.href = "./index.html";
+}
